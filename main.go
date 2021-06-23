5e2534d67f36e73481e6a66fa99e3d9d34321bce
zjh <zoujunhui@bilibili.co>
2021-06-23 17:11:25 +0800

    fix

--- main.go
+++ main.go
@@ -2,19 +2,18 @@ package main
 
 import (
 	"net/http"
-	"github.com/gin-gonic/gin"
-	)
 
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	// 1.创建路由
 	r := gin.Default()
 	// 2.绑定路由规则，执行的函数
-	// gin.Context，封装了request和response
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "hello World!")
 	})
 	// 3.监听端口，默认在8080
-	//Run("里面不指定端口号默认为8080")
-	_ = r.Run(":8000")
+	//r.Run("里面不指定端口号默认为8080")
+	_ = r.Run("8000")
 }
